caccc3fad1b4bd1f79dbb24280540df7384cfba0
Haripriyaremesh <ramdikhari1@gmail.com>
2016-10-07 11:27:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one', function (req, res) {
+  res.send('Article-one is requested');
+});
+app.get('/article-Two', function (req, res) {
+  res.send('Article-Two is requested');
+});
+app.get('/article-Three', function (req, res) {
+  res.send('Article-Three is requested');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
