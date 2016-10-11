38e4c711e4ab26d49585a5057d30b60dbe92ea38
Haripriyaremesh <ramdikhari1@gmail.com>
2016-10-11 20:15:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,8 +55,39 @@ function createTemplate(data)
   
   var htmlTemplate=
     `<html>
-    </html>`;
+     <head>
+     <title>
+       ${title}
+     </title>
+     <meta name="viewport" content="width=device-width,initial-scale=1"/>
+       <link href="/ui/style.css" rel="stylesheet" />
+     </head>
+     <body>
+     <div class="container">
+     <div>
+         <a href="/">Home</a>
+     </div>
+     <hr/>
+     <div>
+         <h3>${heading}</h3>
+     </div>
+     <div>
+           ${date}
+     </div>
+     <div>
+        ${content}
+     </div>
+     </div>
+ </body>
+</html>
+`;
 }
+
+app.get(':articleName', function (req, res) {
+
+  var articleName=req.params.articleName;
+  res.send(createTemplate(article[articleName]));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
