f13833a90aed4b013ec6f993b359a2060af5b5a4
Haripriyaremesh <ramdikhari1@gmail.com>
2016-10-13 21:47:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,16 +3,16 @@ var element= document.getElementById('main-text');
 element.innerHTML="new value";
 
 //image to move
-var img=document.getElementById('modi');
+/*var img=document.getElementById('modi');
 var marginLeft=0;
 function moveRight()
 {
   marginLeft=marginLeft+10;
-  img.style.marginLeft=marginLeft+ 'px';
+  img.style.marginLeft= marginLeft+ 'px';
   
-}  
+}  */
 img.onclick=function()
 {
- 
-  var interval=setInterval(MoveRight,50);  
+  img.style.marginLeft='100px';
+  //var interval=setInterval(MoveRight,50);  
 };
