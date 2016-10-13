35bce7159a7f5c809034075841c5929abb88a7a3
Haripriyaremesh <ramdikhari1@gmail.com>
2016-10-13 21:19:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,17 @@
 console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML="new value";
+
+//image to move
+
+var marginLeft=0;
+function moveRight()
+{
+  marginLeft=marginLeft+10;
+  img.style=marginLeft+ 'px';
+  
+}
+img.onclick=function()
+{
+  var interval=setInterval(MoveRight,100);  
+};
